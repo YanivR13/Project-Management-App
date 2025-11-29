3822993f05c090093f6ad8345f2b92550eef462d
Michal Rochlin <Michal Rochlin@DESKTOP-3OPD8S3.lan>
2025-11-29 16:26:35 +0200

    Change from PC B

--- Prototype/src/Test.java
+++ Prototype/src/Test.java
@@ -3,5 +3,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Test from Computer A");
         System.out.println("Check number 3");
+        System.out.println("Check number 4");
     }
 }
