ae6c1b33e8847ef0a1eca59ba6a2468ffc76b68f
YanivR13 <132251678+YanivR13@users.noreply.github.com>
2025-11-29 15:24:01 +0200

    Check number 3, pressed button now is only Commit (not Commit and Push)

--- Prototype/src/Test.java
+++ Prototype/src/Test.java
@@ -2,6 +2,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Test from Computer A");
-        System.out.println("Check number 2");
+        System.out.println("Check number 3");
     }
 }
