34d4634dfa2f849e6301680fe61a7ac46e7f9111
YanivR13 <132251678+YanivR13@users.noreply.github.com>
2025-11-29 16:48:51 +0200

    Final Sync Check from PC A

--- Prototype/src/Test.java
+++ Prototype/src/Test.java
@@ -4,5 +4,6 @@ public class Test {
         System.out.println("Test from Computer A");
         System.out.println("Check number 3");
         System.out.println("Check number 4");
+        System.out.println("Check number 5");
     }
 }
